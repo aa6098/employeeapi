7e9dd161614cee7cdb7d8336527a070fbc18b27f
arjun <aa6098@hotmail.com>
2026-01-19 21:52:34 -0500

    test

---
+++ src/main/java/com/processonwheels/employeeapi/entity/Employee.java
@@ -0,0 +1,18 @@
+package com.processonwheels.employeeapi.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
